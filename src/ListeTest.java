ce8174e02ddc8489a28b2f0a557b166426fb67ff
Mikkel Bentsen <cph-mb781@cphbusiness.dk>
2021-09-07 10:38:29 +0200

    insertFromHead rework

--- src/ListeTest.java
+++ src/ListeTest.java
@@ -8,6 +8,11 @@ class ListeTest
     @org.junit.jupiter.api.Test
     void insertFromHead()
     {
+        Node node = new Node("2");
+        Node node1 = new Node("3");
+
         assertEquals("1",liste.insertFromHead(new Node("1")).data);
+        assertEquals("2",liste.insertFromHead(node).data);
+        assertEquals("3",liste.insertFromHead(node1).data);
     }
 }
